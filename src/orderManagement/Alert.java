d4dd3950e49e3751ceecbb76d96012bf7780e662
健太 <hksngtw9545@gmail.com>
2015-11-09 01:06:37 +0900

    delete用のメソッドを削除

--- src/orderManagement/Alert.java
+++ src/orderManagement/Alert.java
@@ -4,11 +4,8 @@ public class Alert {
   public static void fraudNumber() {
     System.out.println("入力データが不正です");
   }
+  
   public static void emptySelect() {
     System.out.println("該当する顧客の受注は存在しません");
-  }
-  public static void resultDelete(boolean deleted) {
-    if (deleted) System.out.println("受注を削除しました");
-    else System.out.println("該当する受注No.は存在しません");
   }
 }
